f0aaf08e6a68f43ec5063d8027782a033fdc030b
박상원 <psw20481@gmail.com>
2025-03-20 21:57:34 +0900

    컴포넌트 생성 #1

--- kickon-fe/src/components/Footer/footer.jsx
+++ kickon-fe/src/components/Footer/footer.jsx
@@ -1,4 +1,39 @@
 import React, { Component } from 'react';
+import { FooterContainer, FooterContent, FooterLogo, FooterTextWrapper, FooterMenu, FooterText } from './footer.style';
+import LogoImage from "../../assets/Logo.svg"; // 로고 이미지 경로
+
+class Footer extends Component {
+    render() {
+        return (
+            <FooterContainer>
+                <FooterContent>
+                    {/* 로고 */}
+                    <FooterLogo>
+                        <img src={LogoImage} alt="Kick-On Logo" />
+                    </FooterLogo>
+
+                    {/* 텍스트와 메뉴 */}
+                    <FooterTextWrapper>
+                        <FooterMenu>
+                            <a href="#">서비스 이용약관</a> | <a href="#">개인정보처리방침</a>
+                        </FooterMenu>
+                        <FooterText>
+                            Copyright 2025. Kick-on. All pictures cannot be copied without permission
+                            &nbsp;&nbsp; E-mail: business.kickon@gmail.com
+                        </FooterText>
+                    </FooterTextWrapper>
+                </FooterContent>
+            </FooterContainer>
+        );
+    }
+}
+
+export default Footer;
+
+
+
+/*
+import React, { Component } from 'react';
 
 class Footer extends Component {
     render() {
@@ -11,3 +46,4 @@ class Footer extends Component {
 }
 
 export default Footer;
+*/
---
+++ kickon-fe/src/components/Footer/footer.style.js
@@ -0,0 +1,40 @@
+// footer.style.js
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 13.125rem; /* 210px -> rem 변환 (210px ÷ 16 = 13.125rem) */
+  padding: 0 46.4375rem 0 17.5rem; /* Figma 여백 (743px ÷ 16, 280px ÷ 16) */
+  background-color: #fff; /* 배경색 */
+`;
+
+export const FooterLogo = styled.div`
+  img {
+    height: auto;
+    max-height: 100%; /* 이미지가 영역을 초과하지 않도록 설정 */
+    display: block;
+  }
+`;
+
+export const FooterMenu = styled.nav`
+  font-size: 0.875rem; /* 약 14px (14px ÷ 16 = 0.875rem) */
+  margin-bottom: 0.5rem;
+
+  a {
+    text-decoration: none;
+    color: #000; /* 검정색 텍스트 */
+    margin-right: 0.625rem; /* 약간의 간격 추가 (10px ÷ 16 = 0.625rem) */
+
+    &:hover {
+      text-decoration: underline; /* 링크에 호버 효과 */
+    }
+  }
+`;
+
+export const FooterText = styled.p`
+  font-size: 0.75rem; /* 약 12px (12px ÷ 16 = 0.75rem) */
+  color: #666; /* 회색 텍스트 */
+`;
