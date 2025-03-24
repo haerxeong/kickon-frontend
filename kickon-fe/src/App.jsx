3471de4f3c7704adcba0874a06549ddef5726761
haerxeong <haeryeong04@naver.com>
2025-03-24 22:51:27 +0900

    Fix: app.jsx 수정

--- kickon-fe/src/App.jsx
+++ kickon-fe/src/App.jsx
@@ -1,20 +1,42 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RootLayout from "./layout/root-layout";
 import Home from "./pages/Home/home";
 import Community from "./pages/Community/community.jsx";
 import News from "./pages/News/news.jsx";
+import NotFound from "./pages/not-found";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootLayout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "/community",
+                element: <Community />,
+            },
+            {
+                path: "/news",
+                element: <News />,
+            },
+        ],
+    },
+]);
 
 function App() {
     return (
         <Router>
-            <RootLayout>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/community" element={<Community />} />
-                    <Route path="/news" element={<News />} />
-                </Routes>
-            </RootLayout>
+            <Routes>
+                <Route path="/" element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path="community" element={<Community />} />
+                    <Route path="news" element={<News />} />
+                </Route>
+            </Routes>
         </Router>
     )
 }
