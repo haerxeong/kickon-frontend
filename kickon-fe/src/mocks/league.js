32e59c9e10698193dec234ec62142ad5e7b0e6c5
haerxeong <haeryeong04@naver.com>
2025-03-27 19:26:06 +0900

    Feat: 리그 목데이터 생성

---
+++ kickon-fe/src/mocks/league.js
@@ -0,0 +1,26 @@
+export const League = [{
+    "code": "SUCCESS",
+    "message": "성공",
+    "data": [
+        {
+            "pk": 1,
+            "enName": "K League 1",
+            "krName": "K리그 1",
+            "logoUrl": "https://upload.wikimedia.org/wikipedia/en/thumb/6/6a/K_League_1_logo.svg/1200px-K_League_1_logo.svg.png",
+            "leagueType": "League"
+        },
+        {
+            "pk": 2,
+            "enName": "Premier League",
+            "krName": "프리미어 리그",
+            "logoUrl": "https://upload.wikimedia.org/wikipedia/en/f/f2/Premier_League_Logo.svg",
+            "leagueType": "League"
+        }
+    ],
+    "meta": {
+        "currentPage": 1,
+        "pageSize": 2,
+        "totalItems": 2,
+        "totalPages": 1
+    }
+}]
