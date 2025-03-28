a58c5f8be94df84312b0fc360dace65c6342c9c7
2ivii <dbswjdals2580@gmail.com>
2025-03-29 03:22:48 +0900

    Feat: TopNews 컴포넌트 제작

--- kickon-fe/src/components/TopNews/topNews.style.js
+++ kickon-fe/src/components/TopNews/topNews.style.js
@@ -1,25 +1,64 @@
 import styled from "styled-components";
 
 export const TopNewsContainer = styled.div`
-    width: 20.125rem;
-    height: 32.75rem;
+    width: 16.1rem;  /* 20.125rem * 0.8 */
+    height: 26.2rem;  /* 32.75rem * 0.8 */
     flex-shrink: 0;
-    border-radius: 0.625rem;
+    border-radius: 0.5rem;  /* 0.625rem * 0.8 */
     border: 1px solid #DCDCDC;
     background: #FFF;
-    position: relative;
-`
+    display: flex;
+    flex-direction: column;  /* 세로로 정렬 */
+    align-items: center;  /* 중앙 정렬 */
+    position: relative;  /* ContainerTitle을 위해 상대적인 위치 설정 */
+    padding-top: 3.2rem;
+    padding-bottom: 0;
+`;
 
 export const ContainerTitle = styled.div`
     color: #000;
-
-    /* Title/title5_Pre_sb_16px */
     font-family: Pretendard;
-    font-size: 1rem;
+    font-size: 0.8rem;  /* 1rem * 0.8 */
     font-style: normal;
     font-weight: 600;
-    line-height: 1rem; /* 100% */
+    line-height: 0.8rem;  /* 1rem * 0.8 */
     position: absolute;
-    top: 1.5rem;
-    left: 1rem;
+    top: 1.2rem;  /* 1.5rem * 0.8 */
+    left: 0.8rem;  /* 1rem * 0.8 */
+`;
+
+export const TNews = styled.div`
+    display: flex;
+    height : 4.5rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;  /* TNews 간의 간격을 설정 */
+    width: 100%;  /* 폭을 100%로 설정하여 균등하게 나열 */
+    position: relative;
+`;
+
+export const NewsImage = styled.img`
+    position: absolute;
+    left: 0.8rem;  /* 0.8rem 위치에 배치 */
+    top: 0;  /* TNews 상단에 위치하도록 설정 */
+    object-fit: cover;  /* 이미지 비율을 유지하며 영역에 맞게 잘라서 표시 */
+`;
+
+export const NewsTitle = styled.div`
+    position: absolute;
+    left: 5.2rem;
+    display: flex;
+    width: 9.6rem;  /* 12rem * 0.8 */
+    height: 3rem;  /* 3.75rem * 0.8 */
+    flex-direction: column;
+    justify-content: center;
+    flex-shrink: 0;
+    color: #000;
+
+    /* Body/body6_Pre_m_13px */
+    font-family: Pretendard;
+    font-size: 0.65rem;  /* 0.8125rem * 0.8 */
+    font-style: normal;
+    font-weight: 500;
+    line-height: 0.9rem;  /* 1.125rem * 0.8 */
 `
