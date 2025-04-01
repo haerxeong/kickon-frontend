05bda0bf5dcfcd47aceb5083822f08c4894c56cd
박상원 <psw20481@gmail.com>
2025-04-01 10:19:47 +0900

    rem수정

--- kickon-fe/src/components/CommunityBoard/CommunityBoard.style.js
+++ kickon-fe/src/components/CommunityBoard/CommunityBoard.style.js
@@ -5,7 +5,7 @@ import ProfileSvg from "../../assets/profile.svg";
 export const CommunityBoardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 33rem;
+  width: 30rem;
   padding: 1.875rem 1rem 1rem 1rem;
   justify-content: flex-start;
   align-items: center;
