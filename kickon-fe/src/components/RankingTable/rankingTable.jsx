0bcd140b95218e9d480d8419fed42d12bb1662e6
haerxeong <haeryeong04@naver.com>
2025-04-06 07:18:46 +0900

    Refactor: 랭킹테이블 스타일 수정

--- kickon-fe/src/components/RankingTable/rankingTable.jsx
+++ kickon-fe/src/components/RankingTable/rankingTable.jsx
@@ -1,71 +1,91 @@
-import React, { useState } from "react";
-import {
-    TableContainer,
-    Table,
-    TableHeader,
-    TableRow,
-    TableData,
-    Title,
-    Divider,
-    LeagueSelector,
-    DropdownArrow,
-    TeamCell,
-    TeamLogo,
-    TeamName,
-    HeaderRow
-} from "./rankingTable.style";
+import React, {useState} from "react";
+import * as S from "./rankingTable.style";
+import {League} from "../../mocks/league.js";
+import { IoChevronDown } from "react-icons/io5";
 
 const RankingTable = ({ title, rankings, type = "season" }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [selectedLeague, setSelectedLeague] = useState("K리그 1");
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     };
 
-    return (
-        <TableContainer>
-            <Title>{title}</Title>
-            <Divider />
-            <LeagueSelector onClick={toggleDropdown}>
-                K리그 1 <DropdownArrow isOpen={isOpen}>▼</DropdownArrow>
-            </LeagueSelector>
-            <Divider />
+    const handleLeagueSelect = (league) => {
+        setSelectedLeague(league.krName);
+        setIsOpen(false);
+    };
 
-            <Table>
+    return (
+        <S.TableContainer>
+            <S.Title>{title}</S.Title>
+            <S.Divider />
+            <S.LeagueSelector onClick={toggleDropdown}>
+                <span>{selectedLeague}</span>
+                <IoChevronDown size="0.7rem" color="#8F8F8F" />
+            </S.LeagueSelector>
+
+            {isOpen && (
+                <S.DropdownMenu>
+                    {League.map(league => (
+                        <S.TabOption key={league.pk} onClick={() => handleLeagueSelect(league)}>
+                            <S.LeagueImage src={league.logoUrl} alt={league.krName} />
+                            <S.LeagueName>{league.krName}</S.LeagueName>
+                        </S.TabOption>
+                    ))}
+                </S.DropdownMenu>
+            )}
+
+            <S.Divider />
+
+            <S.Table>
                 <thead>
-                <HeaderRow>
-                    <TableHeader align="left">순위</TableHeader>
-                    <TableHeader align="center"></TableHeader>
+                <S.HeaderRow type={type}>
+                    <S.TableHeader align="left">순위</S.TableHeader>
+                    <S.TableHeader align="center"></S.TableHeader>
                     {type === "season" ? (
                         <>
-                            <TableHeader align="center">경기</TableHeader>
-                            <TableHeader align="center">승점</TableHeader>
-                            <TableHeader align="center">득점</TableHeader>
+                            <S.TableHeader align="center">경기</S.TableHeader>
+                            <S.TableHeader align="center">승점</S.TableHeader>
+                            <S.TableHeader align="center">득점</S.TableHeader>
                         </>
                     ) : (
                         <>
-                            <TableHeader align="center">경기</TableHeader>
-                            <TableHeader align="center">점수</TableHeader>
+                            <S.TableHeader />
+                            <S.TableHeader align="center">경기</S.TableHeader>
+                            <S.TableHeader align="center">점수</S.TableHeader>
                         </>
                     )}
-                </HeaderRow>
+                </S.HeaderRow>
                 </thead>
                 <tbody>
                 {rankings.slice(0, 10).map((item, index) => (
-                    <TableRow key={index}>
-                        <TableData>{item.rankOrder}</TableData>
-                        <TeamCell>
-                            <TeamLogo src={item.teamLogoUrl} alt={`${item.teamName} logo`} />
-                            <TeamName>{item.teamName}</TeamName>
-                        </TeamCell>
-                        <TableData>{item.gameNum}</TableData>
-                        <TableData>{item.points}</TableData>
-                        {type === "season" && <TableData>{item.wonScores}</TableData>}
-                    </TableRow>
+                    <S.TableRow key={index}>
+                        <S.TableData>{item.rankOrder}</S.TableData>
+
+                        <S.TeamCell>
+                            <S.TeamLogo src={item.teamLogoUrl} alt={`${item.teamName} logo`} />
+                            <S.TableData>{item.teamName}</S.TableData>
+                        </S.TeamCell>
+
+                        {type === "season" ? (
+                            <>
+                                <S.TableData>{item.gameNum}</S.TableData>
+                                <S.TableData>{item.points}</S.TableData>
+                                <S.TableData>{item.wonScores}</S.TableData>
+                            </>
+                        ) : (
+                            <>
+                                <S.TableData />
+                                <S.TableData>{item.gameNum}</S.TableData>
+                                <S.TableData>{item.points}</S.TableData>
+                            </>
+                        )}
+                    </S.TableRow>
                 ))}
                 </tbody>
-            </Table>
-        </TableContainer>
+            </S.Table>
+        </S.TableContainer>
     );
 };
 
