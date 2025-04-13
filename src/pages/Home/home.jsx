bf609177404c541ebf1e3f4bef2bd747bd3c9c88
haerxeong <haeryeong04@naver.com>
2025-04-13 18:15:50 +0900

    Refactor: 사용하지 않는 코드 제거

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,19 +2,8 @@ import MatchCard from "../../components/MatchCard/matchCard";
 import CommunityBoard from "../../components/CommunityBoard/communityBoard";
 import NewsList from "../../components/NewsList/newsList";
 import * as S from "./home.style";
-import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
 
 const Home = () => {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const accessToken = localStorage.getItem('accessToken');
-        if (accessToken) {
-            console.log('Access Token:', accessToken);
-        }
-    }, [navigate]);
-
     return (
         <>
             <MatchCard/>
