c69552ef16e0ed83207589c58f1b8c4c2b1c0c0d
haerxeong <haeryeong04@naver.com>
2025-04-29 15:54:46 +0900

    Fix: s3 put 수정

--- src/utils/imageUpload.js
+++ src/utils/imageUpload.js
@@ -1,10 +1,5 @@
-import axiosInstance from "../apis/axios-instance.js"
+import axiosInstance from "../apis/axios-instance.js";
 
-/**
- * presigned URL을 발급받아 S3에 파일 업로드 후, 접근 가능한 이미지 URL을 반환합니다.
- * @param {File} file - 업로드할 파일 객체
- * @returns {Promise<string>} - 업로드된 이미지의 S3 URL
- */
 export const uploadImageToS3 = async (file) => {
     try {
         // presigned URL 요청
@@ -21,8 +16,8 @@ export const uploadImageToS3 = async (file) => {
             }
         );
 
-        // presigned URL, 실제 접근 URL 추출 (API 응답 구조에 따라 key 수정)
-        const { presignedUrl, fileUrl } = presignedRes.data;
+        // presigned URL, 실제 접근 URL 추출
+        const { presignedUrl, sUrl } = presignedRes.data.data;
 
         // S3에 파일 업로드 (PUT)
         await axiosInstance.put(
@@ -33,13 +28,12 @@ export const uploadImageToS3 = async (file) => {
                     "Content-Type": file.type,
                     "x-amz-acl": "public-read",
                 },
-                // baseURL 무시 필요시:
-                baseURL: "", // presignedUrl은 절대경로이므로 axiosInstance의 baseURL 무시
+                baseURL: "", // presignedUrl은 절대경로
             }
         );
 
         // 업로드된 파일 URL 반환
-        return fileUrl;
+        return sUrl;
     } catch (error) {
         console.error("이미지 S3 업로드 실패:", error);
         throw error;
