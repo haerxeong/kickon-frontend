57a4bc75f707b7f95031cd6c767bb5541e6b7ad8
haerxeong <haeryeong04@naver.com>
2025-04-30 16:41:12 +0900

    Refactor: 디버깅을 위한 출력 추가

--- src/utils/imageUpload.js
+++ src/utils/imageUpload.js
@@ -1,42 +1,52 @@
 import axiosInstance from "../apis/axios-instance.js";
+import axios from "axios";
 
 export const uploadImageToS3 = async (file) => {
     try {
-        // presigned URL 요청
+        // Presigned URL 요청
         const presignedRes = await axiosInstance.post(
             "/api/aws/presigned-url",
             {
                 type: "news-images",
                 fileName: file.name,
-            },
-            // {
-            //     headers: {
-            //         "x-amz-acl": "public-read",
-            //     },
-            // }
-        );
-
-        // presigned URL, 실제 접근 URL 추출
-        console.log(presignedRes)
-        const { presignedUrl, sUrl } = presignedRes.data;
-
-        // S3에 파일 업로드 (PUT)
-        await axiosInstance.put(
-            presignedUrl,
-            file,
-            {
-                headers: {
-                    "Content-Type": file.type,
-                    "x-amz-acl": "public-read",
-                },
-                baseURL: "", // presignedUrl은 절대경로
             }
         );
 
+        console.log("Presigned Response Data:", presignedRes.data);
+
+        const { presignedUrl, s3Url } = presignedRes.data;
+
+        if (!presignedUrl || !s3Url) {
+            throw new Error("Presigned URL 또는 s3Url 데이터가 누락되었습니다.");
+        }
+
+        console.log("Presigned URL:", presignedUrl);
+        console.log("Actual File URL (s3Url):", s3Url);
+
+        // S3에 파일 업로드
+        console.log("File Info:", {
+            name: file.name,
+            type: file.type,
+            size: file.size,
+        });
+
+        await axios.put(
+            presignedUrl, // Presigned URL 그대로 사용
+            file,
+            {
+                headers: {
+                    "Content-Type": file.type || "application/octet-stream",
+                    "x-amz-acl": "public-read",
+                },
+            }
+        );
+
+        console.log("파일 업로드 성공:", s3Url);
+
         // 업로드된 파일 URL 반환
-        return sUrl;
+        return s3Url;
     } catch (error) {
-        console.error("이미지 S3 업로드 실패:", error);
+        console.error("이미지 S3 업로드 실패:", error.response?.data || error.message);
         throw error;
     }
 };
