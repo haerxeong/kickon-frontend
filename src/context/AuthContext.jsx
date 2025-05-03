60a693f3d45d8b18412080cae996308f3fad4eb6
haerxeong <haeryeong04@naver.com>
2025-05-04 02:01:04 +0900

    Fix: 토큰 관련 문제 수정

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,31 +1,57 @@
 import React, { createContext, useState, useEffect } from "react";
+import { fetchUserInfo } from "../apis/domains/auth/fetchUserInfo";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // 인증 여부 로딩 완료 여부
 
-  useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      setIsAuthenticated(true);
-    }
-  }, []);
-
-  const login = () => {
+  const login = (userData) => {
+    setUser(userData);
     setIsAuthenticated(true);
   };
 
   const logout = () => {
     localStorage.removeItem("accessToken");
     localStorage.removeItem("refreshToken");
+    setUser(null);
     setIsAuthenticated(false);
   };
 
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = localStorage.getItem("accessToken");
+      if (!token) {
+        setLoading(false);
+        return;
+      }
+
+      const refreshToken = localStorage.getItem("refreshToken");
+      if (!token || !refreshToken) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const userData = await fetchUserInfo();
+        login(userData);
+      } catch (err) {
+        console.warn("토큰 유효하지 않음, 로그아웃 처리", err);
+        logout();
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login , logout}}>
-      {children}
-    </AuthContext.Provider>
+      <AuthContext.Provider value={{ isAuthenticated, login, logout, user, loading }}>
+        {!loading && children}
+      </AuthContext.Provider>
   );
 };
 
