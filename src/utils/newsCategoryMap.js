9b44739fcf670663d1e1236fb93119b801d6edd3
haerxeong <haeryeong04@naver.com>
2025-05-06 00:19:25 +0900

    Fix: 카테고리 영문 정정

--- src/utils/newsCategoryMap.js
+++ src/utils/newsCategoryMap.js
@@ -1,12 +1,12 @@
 const newsCategoryMap = {
     "부상": "INJURY",
     "이적": "TRANSFER",
-    "감독 교체": "MANAGER_CHANGE",
+    "감독 교체": "HEADCOACH",
     "재계약": "RENEWAL",
-    "불화설": "CONFLICT",
-    "은퇴": "RETIREMENT",
+    "불화설": "UNHAPPY",
+    "은퇴": "RETIRE",
     "인터뷰": "INTERVIEW",
-    "현지 팬 반응": "FAN_REACTION",
+    "현지 팬 반응": "LOCAL",
     "기타": "ETC",
 };
 
