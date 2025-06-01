2a029f43558660bb23782d7c3b7d719189551723
haerxeong <haeryeong04@naver.com>
2025-06-01 21:59:14 +0900

    Feat: 프로필 이미지 변경

--- src/apis/domains/auth/updateUserInfo.js
+++ src/apis/domains/auth/updateUserInfo.js
@@ -3,19 +3,20 @@ import axiosInstance from "../../axios-instance.js";
 export const updateUserInfo = async (body) => {
 	try {
 		const token = localStorage.getItem("accessToken");
-		const response = await axiosInstance.patch('/api/user', body, {
+		const response = await axiosInstance.patch("/api/user", body, {
 			headers: {
-				Authorization: `Bearer ${token}`, // Add the token to the Authorization header
+				Authorization: `Bearer ${token}`,
 			},
 		});
 
-		if (!response.code.split('_').includes('SUCCESS')) {
+		if (!response.code?.includes('SUCCESS')) {
 			console.error(response);
 			return response.message;
 		}
+
 		return response;
 	} catch (error) {
-		console.error('유저 정보 수정 실패: ', error);
+		console.error("유저 정보 수정 실패: ", error);
 		throw error;
 	}
 };
